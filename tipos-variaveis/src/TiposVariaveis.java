1c3dd2360200f8091c07b7691f302c848de05b5f
Lucas <lucastheodoromourofernandes@gmail.com>
2025-01-30 21:35:52 -0400

    Variaveis x Variaveis Constantes e também entendendo um pouco de Strings

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,16 +1,18 @@
 public class TiposVariaveis {
     public static void main(String[] args)
     {
-        byte idade = 19;
-        short ano = 2025;
-        int cep = 21070333; // se começar  com  zero, talvez tenha que mudar de tipo
-        long cpf = 98765432109L; // se começar  com  zero, talvez tenha que mudar de tipo
-        float pi = 3.14f;
-        double salario = 1500.76;
-        char sexo =  'M';
-        salario = 2000; // salario  vai ser 2000
+        // entendendo a diferença entre variavel e variavel constante
 
-        System.out.println(salario);
+        //variavel
+        int valor = 5;
+        valor = 10;
+        System.out.println(valor); // vai aparecer valor 10, pq nós fez a troca de valor :)
 
+        // constante
+        final double  VALOR_DE_PI = 100; // para fins de boa prática, todas as constantes tem que em CAPSLOCK
+        //VALOR_DE_PI = 100; // Se nós tentar trocar o valor de pi vai dar erro, pq ela é uma constante
+        System.out.println(VALOR_DE_PI);
+        String nome = "TheodoroL"; // representação de um texto
+        System.out.println(nome);
     }
 }
