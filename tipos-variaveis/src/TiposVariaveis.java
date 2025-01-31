e52f2e10198211ed3bbffdf00538b62c9fa69e61
Lucas <lucastheodoromourofernandes@gmail.com>
2025-01-30 21:25:00 -0400

    Entendendo sobre os tipos primitivos e também entendendo sobre variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,16 @@
 public class TiposVariaveis {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        byte idade = 19;
+        short ano = 2025;
+        int cep = 21070333; // se começar  com  zero, talvez tenha que mudar de tipo
+        long cpf = 98765432109L; // se começar  com  zero, talvez tenha que mudar de tipo
+        float pi = 3.14f;
+        double salario = 1500.76;
+        char sexo =  'M';
+        salario = 2000; // salario  vai ser 2000
+
+        System.out.println(salario);
+
     }
 }
