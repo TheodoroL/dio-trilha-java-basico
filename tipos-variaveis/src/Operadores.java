cc4117bec37e0c064587e7f99ef4f897701b3c9d
Lucas <lucastheodoromourofernandes@gmail.com>
2025-01-31 17:28:16 -0400

    Entendendo Operadores Unários (Positivo e Negativo)

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,15 +1,15 @@
 public class Operadores {
     public static void main(String[] args) {
-        String concatenacao = "?";
+        int numero = 5;
+        System.out.println(-numero); // -5
+        System.out.println(numero); // o resultado vai ser 5, pois o - não afetou a nossa variavel
 
-        concatenacao = 1+1+1+"1"; //31
-        System.out.println(concatenacao);
+        // Porém se eu fazer isso:
+        numero = -numero;
+        System.out.println(numero); // o resultado vai ser -5, pois o - afetou a nossa variavel
 
-        concatenacao = 1+"1"+1+1;//1111
-        System.out.println(concatenacao);
-
-        concatenacao = 1+"1"+1+"1"; // 1111
-        System.out.println(concatenacao);
-
+        // para tornar ele positivo novamente, basta fazer isso :
+        numero = numero*-1;
+        System.out.println(numero);
     }
 }
