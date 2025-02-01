7d52a028a7e1bde01aedfd95b66da5cb979ba952
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-01 19:17:40 -0400

    Entendendo estrutura de condição ternária

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -5,17 +5,9 @@ public class ResultadoEscolar {
         var scanner = new Scanner(System.in);
         System.out.print("digita a sua nota:");
         int nota = scanner.nextInt();
-        if(nota >= 7){
-            System.out.println("Aprovado");
-        }
-        else if( nota >=5 && nota < 7){
-            System.out.println("Prova de recuperação");
-
-        }
-        else{
-            System.out.println("Reprovado");
-
-        }
+        //Operador Ternário
+        String situacao = nota >=7 ? "Aprovado": nota>=5 && nota <7 ? "Prova de exame":"Reprovado";
+        System.out.println(situacao);
         scanner.close();
     }
 }
