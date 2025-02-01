2f1828b2899a9c6250623ab5647ed6a5c493f85c
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-01 19:11:51 -0400

    Entendendo estrutura condcional encadeada

--- controle-de-fluxo/src/ResultadoEscolar.java
+++ controle-de-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
+
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 3;
+        var scanner = new Scanner(System.in);
+        System.out.print("digita a sua nota:");
+        int nota = scanner.nextInt();
         if(nota >= 7){
             System.out.println("Aprovado");
+        }
+        else if( nota >=5 && nota < 7){
+            System.out.println("Prova de recuperação");
+
         }
         else{
             System.out.println("Reprovado");
 
         }
+        scanner.close();
     }
 }
