cd1822eec8d81df93efabe8531490368a124fc11
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-02 16:35:26 -0400

    Aprendendo estrutura de repetição for

---
+++ controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+
+    public static void main(String[] args) {
+        for(int count = 1; count <= 20; count++){
+            System.out.printf("Contando carneirinho %d\n", count);
+        }
+
+    }
+}
