707beae438547ffa46c116f0b1cd970ad89622d6
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-02 16:46:20 -0400

    aprendendo usar o for em arrays e também aprendendo usar o foreach

--- controle-de-fluxo/src/ExemploForArray.java
+++ controle-de-fluxo/src/ExemploForArray.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ExemploForArray {
+public class ExemploForArray
+{
+    public static void main(String[] args) {
+        String[] nomes = {"Lucas", "Daniel", "Leonardo", "Andressa"};
+        //for
+        for(int c = 0; c < nomes.length; c++){
+            System.out.printf("O aluno no indice %d é %s\n", c, nomes[c]);
+        }
+        System.out.println("---------------------------");
+        //foreach
+        for (var nome : nomes){
+            System.out.println(nome);
+        }
+
+    }
 }
