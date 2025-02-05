dcf08f6f997e0c43cec828fbf788cac40ffb90b5
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-05 18:32:01 -0400

    Inicio de Orientação ao Objetos e também aprendendo usar os pacotes (package e import) para deixar o nosso projeto mais organizado

---
+++ Orientação ao Objetos/aula-de-pacote/src/com/mastersoft/clincal/model/Cliente.java
@@ -0,0 +1,4 @@
+package com.mastersoft.clincal.model;
+
+public class Cliente {
+}
---
+++ Orientação ao Objetos/aula-de-pacote/src/com/mastersoft/clincal/service/ExameService.java
@@ -0,0 +1,10 @@
+package com.mastersoft.clincal.service;
+
+import com.mastersoft.clincal.model.Exame;
+
+public class ExameService {
+    public static void salvarExame(Exame exame){
+        // lógica de salvar o exame
+        System.out.println("Salvando o exame...");
+    }
+}
