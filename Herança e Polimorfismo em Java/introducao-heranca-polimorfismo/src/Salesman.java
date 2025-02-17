7b938cc86037bf704bbbf2b0be3e2db14f8e0e86
Lucas <lucastheodoromourofernandes@gmail.com>
2025-02-17 14:04:48 -0400

    Comentei errado na parte sobre sealed e non-sealed

--- Herança e Polimorfismo em Java/introducao-heranca-polimorfismo/src/Salesman.java
+++ Herança e Polimorfismo em Java/introducao-heranca-polimorfismo/src/Salesman.java
@@ -1,5 +1,7 @@
 //se você colocar como final uma classe, nem outra classe pode extender ela
-//porém, se eu colocar  non-selead, irei conseguir extender o Employed e também falar que não é possivel extender esas classes
+//Non-Sealed Classes são classes que não possuem a palavra-chave sealed declarada. Essas classes podem ser herdadas por outras classes e podem ter subclasses. É o comportamento padrão em Java.
+//Sealed Classes, por outro lado, são classes que possuem a palavra-chave sealed declarada. Essas classes podem ter subclasses, mas essas subclasses devem ser declaradas explicitamente na classe pai. Isso significa que você não pode criar subclasses de uma classe sealed sem declará-las explicitamente.
+
 public non-sealed class Salesman extends Employed  {
     private double percentPorSold;
 
